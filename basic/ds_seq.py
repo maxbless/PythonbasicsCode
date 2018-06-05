5899717cbc7ca80203bc5a35e9fa2c1191ae423b
maxbless <laiyubin0416@163.com>
2018-06-05 23:02:12 +0800

    更新
    Signed-off-by: maxbless <laiyubin0416@163.com>

---
+++ basic/ds_seq.py
@@ -0,0 +1,16 @@
+shoplist = ['apple','mango','carrot','banana']
+name = 'laiyubin'
+#索引/下标操作符
+print('item[0] is',shoplist[0])
+print('item[1] is',shoplist[1])
+print('item[2] is',shoplist[2])
+print('item[3] is',shoplist[3])
+
+#字段中的部分
+print('item 1 to 3 is',shoplist[1:3])
+print('item 0 to 2 is',shoplist[0:2])
+print('item 1 to -1 is',shoplist[1:-1])
+
+#从某一字符串中截取
+print('1 to 30 is',name[1:3])
+print('2 to -1 is',name[2:-1])#-1不包括最后一项
